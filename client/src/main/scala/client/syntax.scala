7d418f2f6e8e54f88e793e36ee6da7f097618d78
Andrés González <a.gonzalez.terres@gmail.com>
2022-01-15 23:15:53 -0300

    cliente con implementcion futuro

--- client/src/main/scala/client/syntax.scala
+++ client/src/main/scala/client/syntax.scala
@@ -1,41 +1,44 @@
 package client
 
-//import io.netty.util.CharsetUtil.UTF_8
 import sttp.capabilities
-import web.{Endpoint, decodeJson}
-import zio.Task
-//import zhttp.http.{Headers, HttpData, URL}
-//import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
-import zio.ZIO
+import sttp.client3.*
+import sttp.model.Uri
+import web.Endpoint
+import zio.{ZIO, ZManaged}
 import zio.json.JsonEncoder
 
-import sttp.client3.*
-//import org.asynchttpclient.{AsyncHttpClient, DefaultAsyncHttpClient}
-//import zio.Runtime
-//import sttp.client3.asynchttpclient.zio.AsyncHttpClientZioBackend
+import scala.concurrent.Future
 
 object syntax:
+
+  extension [In, Out](self: Endpoint[In, Out])
+    // TODO cambiar la implementacion en futuro a una de cats effects porque futuro no se puede cancelar bien
+    def fetch(input: In): ZIO[Any, Throwable, Out] =
+      (for {
+        uri <- ZIO fromEither Uri.parse(s"http://localhost:8080/${self.route}").left.map(new Exception(_))
+        response <- makeBackend.use(backend =>
+                      ZIO fromFuture { _ =>
+                        sttp.client3.basicRequest
+                          .method(convert(self.method), uri)
+                          .body(self.inCodec.encoder.encodeJson(input, None).toString)
+                          .send(backend)
+                      }
+                    )
+      } yield response.body.flatMap(self.outCodec.decoder.decodeJson).left.map(new Exception(_))).absolve
+
+  private def makeBackend: ZManaged[Any, Nothing, SttpBackend[Future, capabilities.WebSockets]] =
+    ZIO.succeed(FetchBackend()).toManaged(backend => ZIO.fromFuture(_ => backend.close()).ignore)
+
+  private def convert(method: web.Method) = method match {
+    case web.Method.GET  => sttp.model.Method.GET
+    case web.Method.POST => sttp.model.Method.POST
+  }
 
 //  AsyncHttpClientZioBackend.managed().use { backend => ??? }
 
 //  val asyncHttpClient: AsyncHttpClient = DefaultAsyncHttpClient()
 //  val runtime: Runtime[Any]            = Runtime.default
 //  val backend: SttpBackend[Task, Any]  = AsyncHttpClientZioBackend.usingClient(runtime, asyncHttpClient)
-
-//  val backend: SttpBackend[scala.concurrent.Future, Any] = FetchBackend()
-
-  extension [In, Out](self: Endpoint[In, Out])
-    def fetch(input: In): ZIO[Any, Throwable, Out] =
-      (for {
-        uri <- ZIO fromEither sttp.model.Uri.parse(s"http://localhost:8080/${self.route}").left.map(new Exception(_))
-        response <- ZIO fromFutureInterrupt { implicit ec =>
-                      val backend: SttpBackend[scala.concurrent.Future, capabilities.WebSockets] = FetchBackend()
-                      sttp.client3.basicRequest
-                        .post(uri)
-                        .body(self.inCodec.encoder.encodeJson(input, None).toString)
-                        .send(backend)
-                    }
-      } yield response.body.flatMap(self.outCodec.decoder.decodeJson).left.map(new Exception(_))).absolve
 
 //    def fetch(input: In): ZIO[EventLoopGroup & ChannelFactory, Throwable, Out] =
 //      ZIO
@@ -47,8 +50,3 @@ object syntax:
 
 //  private def inputToJsonData[A](encoder: JsonEncoder[A])(input: A) =
 //    HttpData.fromString(encoder.encodeJson(input, None).toString, UTF_8)
-
-//  private def convert(method: web.Method) = method match {
-//    case web.Method.GET  => zhttp.http.Method.GET
-//    case web.Method.POST => zhttp.http.Method.POST
-//  }
