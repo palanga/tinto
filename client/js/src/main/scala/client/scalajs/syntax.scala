386d1e8d02ed18d6e0881e07821247cb143522bf
Andrés González <a.gonzalez.terres@gmail.com>
2022-01-20 01:45:11 -0300

    client v4

--- client/js/src/main/scala/client/scalajs/syntax.scala
+++ client/js/src/main/scala/client/scalajs/syntax.scala
@@ -2,12 +2,60 @@ package client.scalajs
 
 import sttp.capabilities
 import sttp.client3.*
-import sttp.model.Uri
+import sttp.model.Method.isIdempotent
+import sttp.model.{Method, Uri}
 import web.v4.InOutEndpoint
 import zio.json.JsonEncoder
-import zio.{ZIO, ZManaged}
+import zio.{IO, ZIO, ZManaged}
 
 import scala.concurrent.Future
+
+object client:
+  import web.v4.*
+
+  def fetch[In, Out](endpoint: Endpoint[In, Out])(input: In): ZIO[Any, Throwable, Out] =
+    val uri         = Uri.parse(s"http://localhost:8080/${endpoint.route.path}").left.map(new Exception(_))
+    val method      = convert(endpoint.method)
+    val requestBody = encodeRequestBody(endpoint, input)
+    ZIO
+      .fromEither(uri)
+      .flatMap(makeRequest(method, requestBody))
+      .map(decodeResponseBody(endpoint))
+      .absolve
+
+  private def encodeRequestBody[In](endpoint: Endpoint[In, _], input: In): String = endpoint match {
+    case AnyUnitEndpoint(_, _) | OutEndpoint(_, _, _) => ""
+    case InEndpoint(_, _, inCodec)                    => inCodec.encodeJson(input, None).toString
+    case InOutEndpoint(_, _, inCodec, _)              => inCodec.encodeJson(input, None).toString
+  }
+
+  private def decodeResponseBody[In, Out](endpoint: Endpoint[In, Out])(
+    response: Response[Either[String, String]]
+  ): Either[Exception, Out] = endpoint match {
+    case AnyUnitEndpoint(_, _) | InEndpoint(_, _, _) => Right(())
+    case OutEndpoint(_, _, outCodec)                 => response.body.flatMap(outCodec.decodeJson).left.map(Exception(_))
+    case InOutEndpoint(_, _, _, outCodec)            => response.body.flatMap(outCodec.decodeJson).left.map(Exception(_))
+  }
+
+  private def makeRequest(method: Method, body: String)(
+    uri: Uri
+  ): ZIO[Any, Throwable, Response[Either[String, String]]] =
+    makeBackend.use(backend =>
+      ZIO fromFuture { _ =>
+        if (Method.isSafe(method) || body.isBlank)
+          sttp.client3.basicRequest.method(method, uri).send(backend)
+        else
+          sttp.client3.basicRequest.method(method, uri).body(body).send(backend)
+      }
+    )
+
+  private def makeBackend: ZManaged[Any, Nothing, SttpBackend[Future, capabilities.WebSockets]] =
+    ZIO.succeed(FetchBackend()).toManaged(backend => ZIO.fromFuture(_ => backend.close()).ignore)
+
+  private def convert(method: web.Method) = method match {
+    case web.Method.GET  => sttp.model.Method.GET
+    case web.Method.POST => sttp.model.Method.POST
+  }
 
 object syntax:
 
